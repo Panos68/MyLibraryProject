21d04740fa9789e5ed7e0c618ebd216cb4400ab9
Panos68 <panosst68@gmail.com>
2019-10-10 22:17:29 +0200

    Added unit tests for BookToBookDTO converter

---
+++ MyLibraryProject/src/test/java/com.panos.model/converters/BookToBookDTOShould.java
@@ -0,0 +1,31 @@
+package com.panos.model.converters;
+
+import com.panos.model.Book;
+import com.panos.model.dto.BookDTO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BookToBookDTOShould {
+
+    private BookToBookDTO bookToBookDTO = new BookToBookDTO();
+
+    @Test
+    void keepNameAfterConversion() {
+        Book book = new Book();
+        String name = "Book";
+        book.setName(name);
+        BookDTO bookDTO = this.bookToBookDTO.convert(book);
+        assertEquals(bookDTO.getName(), name);
+    }
+
+    @Test
+    void keepIdAfterConversion() {
+        Book book = new Book();
+        Long id = 1L;
+        book.setId(id);
+        BookDTO bookDTO = this.bookToBookDTO.convert(book);
+        assertEquals(bookDTO.getId(), id);
+    }
+
+}
